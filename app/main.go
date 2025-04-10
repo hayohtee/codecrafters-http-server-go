e4ea922d58847484ac2fa2ac73d6aaa331b3e766
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-04-10 23:28:15 +0100

    Add POST endpoint for file uploads

--- app/main.go
+++ app/main.go
@@ -5,17 +5,19 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/textproto"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	var app application
-	flag.StringVar(&app.directory, "directory", "/tmp/", "The directory to check for files")
+	var directory string
+	flag.StringVar(&directory, "directory", "/tmp/", "The directory to check for files")
 	flag.Parse()
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -34,6 +36,10 @@ func main() {
 		}
 		go func() {
 			defer conn.Close()
+			app := application{
+				filesDir: directory,
+			}
+
 			if err := app.handleConn(conn); err != nil {
 				log.Println("error handling connection:", err)
 			}
@@ -42,10 +48,12 @@ func main() {
 }
 
 type application struct {
-	directory string
+	filesDir      string
+	requestURL    string
+	requestMethod string
 }
 
-func (app application) handleConn(conn net.Conn) error {
+func (app *application) handleConn(conn net.Conn) error {
 	tp := textproto.NewReader(bufio.NewReader(conn))
 
 	// Read the request line
@@ -66,17 +74,23 @@ func (app application) handleConn(conn net.Conn) error {
 	}
 
 	path := requestLine[1]
+	app.requestMethod = requestLine[0]
+	app.requestURL = requestLine[1]
 
 	// Handle connection based on url
 	switch {
 	case path == "/":
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n\r\n"))
-	case strings.HasPrefix(path, "/files/"):
-		if err := app.fileHandler(conn, path); err != nil {
+	case strings.HasPrefix(data, "GET /files/"):
+		if err := app.fileHandler(conn); err != nil {
+			return fmt.Errorf("failed to handle request to %s: %w", path, err)
+		}
+	case strings.HasPrefix(data, "POST /files/"):
+		if err := app.postFileHandler(conn, tp); err != nil {
 			return fmt.Errorf("failed to handle request to %s: %w", path, err)
 		}
 	case strings.HasPrefix(path, "/echo/"):
-		if err := app.echoHandler(conn, path); err != nil {
+		if err := app.echoHandler(conn); err != nil {
 			return fmt.Errorf("failed to handle request to %s: %w", path, err)
 		}
 	case path == "/user-agent":
@@ -101,9 +115,41 @@ func (app application) userAgentHandler(conn net.Conn, tp *textproto.Reader) err
 	return nil
 }
 
-func (app application) fileHandler(conn net.Conn, urlPath string) error {
-	filename := strings.TrimPrefix(urlPath, "/files/")
-	path := filepath.Join(app.directory, filename)
+func (app application) postFileHandler(conn net.Conn, tp *textproto.Reader) error {
+	headers, err := tp.ReadMIMEHeader()
+	if err != nil {
+		return err
+	}
+
+	contentLengthStr := headers.Get("Content-Length")
+	contentLength := 0
+	if contentLengthStr != "" {
+		contentLength, err = strconv.Atoi(contentLengthStr)
+		if err != nil {
+			return err
+		}
+	}
+
+	body := make([]byte, contentLength)
+	_, err = io.ReadFull(tp.R, body)
+	if err != nil {
+		return err
+	}
+
+	filename := strings.TrimPrefix(app.requestURL, "/files/")
+	path := filepath.Join(app.filesDir, filename)
+
+	if err = os.WriteFile(path, body, 0644); err != nil {
+		return err
+	}
+	
+	conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
+	return nil
+}
+
+func (app *application) fileHandler(conn net.Conn) error {
+	filename := strings.TrimPrefix(app.requestURL, "/files/")
+	path := filepath.Join(app.filesDir, filename)
 	file, err := os.ReadFile(path)
 	if err != nil {
 		switch {
@@ -119,8 +165,8 @@ func (app application) fileHandler(conn net.Conn, urlPath string) error {
 	return nil
 }
 
-func (app application) echoHandler(conn net.Conn, path string) error {
-	word := strings.TrimPrefix(path, "/echo/")
+func (app application) echoHandler(conn net.Conn) error {
+	word := strings.TrimPrefix(app.requestURL, "/echo/")
 	statusOKResponse(conn, "text/plain", []byte(word))
 	return nil
 }
