de745c92dbcd46387e248641c4b6b46c05fd08bd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-04-10 16:25:36 +0100

    Handle incomming connection and send an HTTP 200 OK response

--- app/main.go
+++ app/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
-
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -16,10 +13,25 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	log.Println("starting server on", l.Addr().String())
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go func() {
+			defer conn.Close()
+			if err := handleConn(conn); err != nil {
+				log.Println("error handling connection:", err)
+			}
+		}()
 	}
+}
+
+func handleConn(conn net.Conn) error {
+	_, err := conn.Write([]byte("HTTP/1.1 200 \r\n\r\n\r\n"))
+	return err
 }
